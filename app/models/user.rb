147383e13bd0496346ea3323e378308ec41b42c3
Brayan <brayansuamon@gmail.com>
2025-02-04 11:41:19 -0500

    Added relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  # Validation to solve the problem of empty name
   validates :name, presence: true
+
+  # Relation to explain user can have many articles
+  has_many :articles
+
   after_create :log_new_user
 
   private
