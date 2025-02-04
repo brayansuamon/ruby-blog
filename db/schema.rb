753a4575daf0df736274c925a6839cb37bff5bc7
Brayan <brayansuamon@gmail.com>
2025-02-04 11:39:41 -0500

    Created new user model with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_30_002602) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_04_162327) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "body"
@@ -28,5 +28,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_30_002602) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "email"
   end
 end
